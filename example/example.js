fb10f5e4eaae4245bf6a0294a0f204c0eb103d88
Michael Bullington <mikebullingtn@gmail.com>
2015-02-08 15:12:28 -0500

    Add node.js implementation of PrivateKey.getSharedSecret().
    - getSharedSecret() is now implemented. Needs tests, and review (mostly to make sure I did all my OpenSSL stuff right).
    - Refactor C++ addon. Makes use of ScopedOpenSSL to avoid complete manual memory management.

--- example/example.js
+++ example/example.js
@@ -1,6 +1,12 @@
 var d = require('../lib/index.js');
-d.generateKeyPair(d.NamedCurve.P256).then(function(result) {
-  console.log(result);
+d.generateKeyPair(d.NamedCurve.P256).then(function(keypair1) {
+  console.log(keypair1);
+  
+  d.generateKeyPair(d.NamedCurve.P256).then(function(keypair2) {
+    console.log(keypair1.privateKey.getSharedSecret(keypair2.publicKey));
+  }).catch(function(e) {
+    console.log(e);
+  });
 }).catch(function(e) {
   console.log(e);
 });
--- src/dhcurve.cc
+++ src/dhcurve.cc
@@ -1,91 +1,143 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "dhcurve.h"
 
-#include <node.h>
-#include <node_buffer.h>
-#include <v8.h>
-
-#include <openssl/bn.h>
-#include <openssl/ec.h>
-#include <openssl/ecdh.h>
-#include <openssl/objects.h>
-
-using namespace node;
-using namespace v8;
-
-#define THROW(error) ThrowException(String::New((error)));
-
-// referenced bignumToBuffer() from ursa, in src/ursaNative.cc L91
-// Copyright 2012 The Obvious Corporation.
 Handle<Value> bnToBuf(const BIGNUM *bn) {
   int bytes = BN_num_bytes(bn);
   Buffer *buffer = Buffer::New(bytes);
   
   if(BN_bn2bin(bn, (unsigned char *) Buffer::Data(buffer)) < 0) {
-    delete buffer;
-    THROW("Failed to create Buffer from BIGNUM.")
+    THROW("Failed to create Buffer from BIGNUM")
     return Undefined();
   }
   
   return buffer->handle_;
 }
 
+BIGNUM *bufToBn(const Handle<Value> buf) {
+  BIGNUM *r;
+  if((r = BN_bin2bn((unsigned char *) Buffer::Data(buf), Buffer::Length(buf), NULL)) == NULL) {
+    THROW("Failed to create BIGNUM from Buffer")
+    return NULL;
+  }
+  return r;
+}
+
+EC_POINT *jsPointToPoint(const EC_GROUP *group, const Handle<Object> obj) {
+  EC_POINT *point = EC_POINT_new(group);
+
+  if(point == NULL) {
+    THROW("Failed to create EC_POINT from Point")
+    return NULL;
+  }
+  
+  S_BIGNUM x(bufToBn(obj->Get(String::New("x"))));
+  
+  if(x.get() == NULL) {
+    THROW("Failed to create BIGNUM from x")
+    return NULL;
+  }
+  
+  S_BIGNUM y(bufToBn(obj->Get(String::New("y"))));
+  
+  if(y.get() == NULL) {
+    THROW("Failed to create BIGNUM from y")
+    return NULL;
+  }
+  
+  ScopedOpenSSL<BN_CTX, BN_CTX_free> ctx(BN_CTX_new());
+  BN_CTX_start(ctx.get());
+  
+  if(EC_POINT_set_affine_coordinates_GF2m(group, point, x.get(), y.get(), ctx.get()) == 0) {
+    THROW("Failed to set coords for EC_POINT")
+    BN_CTX_end(ctx.get());
+    return NULL;
+  }
+  
+  BN_CTX_end(ctx.get());
+  return point;
+}
+
 Handle<Value> GetSharedSecret(const Arguments& args) {
-  return Undefined();
+  String::Utf8Value namedCurve(args[0]);
+  S_EC_KEY key(EC_KEY_new_by_curve_name(OBJ_sn2nid(*namedCurve)));
+  
+  if(key.get() == NULL) {
+    THROW("Key could not be created using curve name")
+    return Undefined();
+  }
+  
+  Local<Value> buf = args[1];
+  S_BIGNUM privateKey(bufToBn(buf));
+  
+  if(privateKey.get() == NULL) {
+    return Undefined();
+  }
+  
+  EC_KEY_set_private_key(key.get(), (const BIGNUM *) privateKey.get());
+  
+  S_EC_POINT peerKey(jsPointToPoint(EC_KEY_get0_group(key.get()), Local<Object>::Cast(args[2])));
+  
+  if(peerKey.get() == NULL) {
+    return Undefined();
+  }
+  
+  int length = Buffer::Length(buf);
+  char* secret = new char[length];
+  
+  if(ECDH_compute_key(secret, length, peerKey.get(), key.get(), NULL) != length) {
+    THROW("Can not compute ECDH shared key")
+    delete secret;
+    return Undefined();
+  }
+  
+  Buffer *r = Buffer::New(secret, length);
+  delete secret;
+  return r->handle_;
 }
 
 Handle<Value> GenerateKeyPair(const Arguments& args) {
   String::Utf8Value namedCurve(args[0]);
-  EC_KEY *key;
   int curve = OBJ_sn2nid(*namedCurve);
   
   if(curve == NID_undef) {
     THROW("Invalid curve name")
   }
   
-  if((key = EC_KEY_new_by_curve_name(curve)) == NULL) {
+  S_EC_KEY key(EC_KEY_new_by_curve_name(curve));
+  
+  if(key.get() == NULL) {
     THROW("Key could not be created using curve name")
-    EC_KEY_free(key);
     return Undefined();
   }
   
-  if(EC_KEY_generate_key(key) == 0) {
+  if(EC_KEY_generate_key(key.get()) == 0) {
     THROW("Key failed to generate")
-    EC_KEY_free(key);
     return Undefined();
   }
   
-  Local<Object> returnValue = Object::New();
+  Local<Object> r = Object::New();
   
-  returnValue->Set(String::New("privateKey"), bnToBuf(EC_KEY_get0_private_key(key)));
+  r->Set(String::New("privateKey"), bnToBuf(EC_KEY_get0_private_key(key.get())));
   
-  const EC_POINT *publicKey = EC_KEY_get0_public_key(key);
-  const EC_GROUP *group = EC_KEY_get0_group(key);
-  BIGNUM *x = BN_new();
-  BIGNUM *y = BN_new();
+  const EC_POINT *publicKey = EC_KEY_get0_public_key(key.get());
+  const EC_GROUP *group = EC_KEY_get0_group(key.get());
+  S_BIGNUM x(BN_new());
+  S_BIGNUM y(BN_new());
   
   if(publicKey == NULL || group == NULL) {
-    BN_clear_free(x);
-    BN_clear_free(y);
-    EC_KEY_free(key);
     THROW("Could not get public key")
     return Undefined();
   }
   
-  EC_POINT_get_affine_coordinates_GFp(group, publicKey, x, y, NULL);
+  EC_POINT_get_affine_coordinates_GFp(group, publicKey, x.get(), y.get(), NULL);
   
   Local<Object> point = Object::New();
   
-  point->Set(String::New("x"), bnToBuf(x));
-  point->Set(String::New("y"), bnToBuf(y));
+  point->Set(String::New("x"), bnToBuf(x.get()));
+  point->Set(String::New("y"), bnToBuf(y.get()));
   
-  returnValue->Set(String::New("publicKey"), point);
+  r->Set(String::New("publicKey"), point);
   
-  BN_clear_free(x);
-  BN_clear_free(y);
-  EC_KEY_free(key);
-  
-  return returnValue;
+  return r;
 }
 
 void Init(Handle<Object> exports, Handle<Value> module) {
