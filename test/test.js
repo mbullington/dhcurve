763bcaf7ac90e3efb1cb0974c7a3b91297a1a786
mbullington <mikebullingtn@gmail.com>
2015-02-15 12:03:14 -0500

    Fully working in browser.
    - generateSharedSecret() now produces correct output.

--- test/test.js
+++ test/test.js
@@ -7,16 +7,15 @@ it('generateKeyPair()', function() {
 
 describe('PrivateKey', function() {
   describe('getSharedSecret()', function() {
-    /*
     it('produces correct output', function() {
       var privateKey = new curve.PrivateKey(curve.NamedCurve.P256, new Buffer('M6S41GAL0gH0I97Hhy7A2-icf8dHnxXPmYIRwem03HE', 'base64'));
       var publicKey = curve.Point.fromEncoded(curve.NamedCurve.P256, new Buffer('BCVrEhPXmozrKAextseekQauwrRz3lz2sj56td9j09Oajar0RoVR5Uo95AVuuws1vVEbDzhOUu7freU0BXD759U', 'base64'));
 
       var secret = privateKey.getSharedSecret(publicKey);
       var secretTest = new Buffer('116128c016cf380933c4b40ffeee8ef5999167f5c3d49298ba2ebfd0502e74e3', 'hex');
+
       assert(secret.toString('hex') === secretTest.toString('hex'));
     });
-    */
 
     it('is reversable', function() {
       var keypair1 = curve.generateKeyPair(curve.NamedCurve.P256);
@@ -31,14 +30,12 @@ describe('PrivateKey', function() {
 });
 
 describe('Point', function() {
-  /*
   it('fromEncoded() & getEncoded()', function() {
     var privateKey = new curve.PrivateKey(curve.NamedCurve.P256, new Buffer('M6S41GAL0gH0I97Hhy7A2-icf8dHnxXPmYIRwem03HE', 'base64'));
     var publicKey = curve.Point.fromEncoded(curve.NamedCurve.P256, new Buffer('BCVrEhPXmozrKAextseekQauwrRz3lz2sj56td9j09Oajar0RoVR5Uo95AVuuws1vVEbDzhOUu7freU0BXD759U', 'base64'));
 
     assert(publicKey.getEncoded().toString('base64').replace("=", "") === 'BCVrEhPXmozrKAextseekQauwrRz3lz2sj56td9j09Oajar0RoVR5Uo95AVuuws1vVEbDzhOUu7freU0BXD759U');
   });
-  */
 
   it('equals()', function() {
     var keypair = curve.generateKeyPair(curve.NamedCurve.P256);
